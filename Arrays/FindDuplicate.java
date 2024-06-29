1855fceb00db4b6f4638fe272f008b0467970e3c
PoojaRaj <poojarj055@gmail.com>
2024-06-29 12:03:45 +0530

    Create FindDuplicate.java
    O(n) is the time complexity O(n) is the space complexity

---
+++ Arrays/FindDuplicate.java
@@ -0,0 +1,21 @@
+//https://www.naukri.com/code360/problems/duplicate-in-array_893397?interviewProblemRedirection=true&leftPanelTabValue=SUBMISSION
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class Solution {
+
+	public static int findDuplicate(ArrayList<Integer> arr) {
+
+		//    Write your code here.
+		int duplicate=0;
+		HashSet<Integer> set=new HashSet<>();
+		for(int num:arr)
+		{
+			if(!set.add(num))
+			duplicate=num;
+		}
+		return duplicate;
+
+	}
+}
