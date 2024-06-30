5209d06b93c20042aae2edb9b1e0ec7ad53d4e4b
PoojaRaj <poojarj055@gmail.com>
2024-06-30 17:38:59 +0530

    Update TwoSum.js

--- Arrays/TwoSum.js
+++ Arrays/TwoSum.js
@@ -1,49 +1,36 @@
-var twoSum = function (arr, target, n) {
-
+var twoSum = function(arr, target) {
     let pairs = [];
-    if (arr.length <= 1) {
-        pairs.push([-1, -1]);
-        return pairs;
+    if (arr.length < 2) {
+        return [[-1, -1]]; // Return early for arrays with less than 2 elements
     }
-
 
     let map = new Map();
-    for (let i = 0; i < arr.length; i++) {
-        if (map.has(arr[i])) {
-            map.set(arr[i], map.get(arr[i])+1)
+    // Populate the map with counts of each number in the array
+    for (let num of arr) {
+        if (map.has(num)) {
+            map.set(num, map.get(num) + 1);
         } else {
-            map.set(arr[i], 1);
+            map.set(num, 1);
         }
     }
 
-
-    for (let i = 0; i < arr.length; i++) {
-        let num = arr[i];
+    // Iterate through the array to find pairs
+    for (let num of arr) {
         let num2 = target - num;
 
-        if (map.has(num2)) {
-            if (map.get(num)) {
-                map.set(num, map.get(num) - 1);
-            } else {
-                break;
-            }
-
-            if (map.get(num2)) {
-                map.set(num2, map.get(num2) - 1);
-            } else {
-                break;
-            }
-
-            pairs.push([num2, num]);
+        if (num === num2 && map.get(num) >= 2) {
+            // Special case: num and num2 are the same and there are at least 2 occurrences
+            pairs.push([num, num2]);
+            map.set(num, map.get(num) - 2); // Decrement count for both occurrences
+        } else if (num !== num2 && map.get(num) > 0 && map.get(num2) > 0) {
+            // General case: num and num2 are different and both are present in the map
+            pairs.push([num, num2]);
+            map.set(num, map.get(num) - 1); // Decrement count for num
+            map.set(num2, map.get(num2) - 1); // Decrement count for num2
         }
     }
 
-    if (pairs.length === 0) {
-        pairs.push([-1, -1]);
-    }
-    
-    return pairs;
-
+    return pairs.length > 0 ? pairs : [[-1, -1]]; // Return pairs or default [-1, -1] if no pairs found
 };
 
 module.exports.solution = twoSum;
