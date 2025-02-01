ef0d408e34e8e585e6f79432d4ec55ec959e348b
PoojaRaj <poojarj055@gmail.com>
2025-02-01 22:32:34 +0530

    Update MajorityElements.java

--- Java_hashmap/MajorityElements.java
+++ Java_hashmap/MajorityElements.java
@@ -33,3 +33,32 @@ class Solution {
         return majorityElement;
     }
 }
+
+// above solution is o(n) time and space complexity
+
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    candidate = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return candidate;
+    }
+}
+
+// space -->> o(1)
+// time -->> o(n)
+
+
+
