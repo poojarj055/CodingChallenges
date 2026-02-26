82121010f2d298d03aaa7c6f10be68f75573626b
PoojaRaj <poojarj055@gmail.com>
2026-02-26 16:53:35 +0530

    Create PopulatingNextRightPointer.java

---
+++ BinaryTree/PopulatingNextRightPointer.java
@@ -0,0 +1,69 @@
+// You are given a perfect binary tree where all leaves are on the same level, and every parent has two children. T
+//he binary tree has the following definition:
+
+// struct Node {
+//   int val;
+//   Node *left;
+//   Node *right;
+//   Node *next;
+// }
+// Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+// Initially, all next pointers are set to NULL.
+// Example 1:
+// Input: root = [1,2,3,4,5,6,7]
+// Output: [1,#,2,3,#,4,5,6,7,#]
+// Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+// Example 2:
+
+// Input: root = []
+// Output: []
+ 
+
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 212 - 1].
+// -1000 <= Node.val <= 1000
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+    public Node connect(Node root) {
+        Node temp=root;
+
+        while(temp!=null && temp.left!=null){
+            Node node=temp;
+
+            while(true){
+                node.left.next=node.right;
+
+                if(node.next==null) break;
+                node.right.next=node.next.left;
+                node=node.next;
+            }
+            temp=temp.left;
+        }
+        return root;
+    }
+}
