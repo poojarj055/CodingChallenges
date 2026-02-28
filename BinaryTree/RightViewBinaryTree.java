2fee0f07895d90c1a414f78a351ea5ba77db1352
PoojaRaj <poojarj055@gmail.com>
2026-02-28 16:07:35 +0530

    Create RightViewBinaryTree.java

---
+++ BinaryTree/RightViewBinaryTree.java
@@ -0,0 +1,67 @@
+// Given the root of a binary tree, imagine yourself standing on the right side of it, return the values of the nodes you can see ordered from top to bottom.
+
+// Example 1:
+
+// Input: root = [1,2,3,null,5,null,4]
+
+// Output: [1,3,4]
+
+// Explanation:
+
+// Example 2:
+
+// Input: root = [1,2,3,4,null,null,null,5]
+
+// Output: [1,3,4,5]
+
+// Explanation:
+// Example 3:
+
+// Input: root = [1,null,3]
+
+// Output: [1,3]
+
+// Example 4:
+
+// Input: root = []
+// Output: []
+// Constraints:
+
+// The number of nodes in the tree is in the range [0, 100].
+// -100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+         List<Integer> rightViewList = new ArrayList<>();
+         rightSideViewRecur(root, 0, rightViewList);
+         return rightViewList;
+    }
+    public void rightSideViewRecur(TreeNode root, int level, List<Integer> rightViewList) {
+        if(root==null){
+            return;
+        }
+      //whenever new size is found then only add in the new list
+        if(rightViewList.size()==level)
+        {
+            rightViewList.add(root.val);
+        }
+        rightSideViewRecur(root.right, level+1, rightViewList);
+        rightSideViewRecur(root.left, level+1, rightViewList);
+    }
+
+}
