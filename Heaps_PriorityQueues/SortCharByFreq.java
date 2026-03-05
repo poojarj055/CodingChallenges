8542af56b17a841c1e331a14ca6f40a481b610c7
PoojaRaj <poojarj055@gmail.com>
2026-03-05 17:12:20 +0530

    Create SortCharByFreq.java

---
+++ Heaps_PriorityQueues/SortCharByFreq.java
@@ -0,0 +1,47 @@
+// Given a string s, sort it in decreasing order based on the frequency of the characters. 
+// The frequency of a character is the number of times it appears in the string.
+
+// Return the sorted string. If there are multiple answers, return any of them.
+// Example 1:
+
+// Input: s = "tree"
+// Output: "eert"
+// Explanation: 'e' appears twice while 'r' and 't' both appear once.
+// So 'e' must appear before both 'r' and 't'. Therefore "eetr" is also a valid answer.
+// Example 2:
+
+// Input: s = "cccaaa"
+// Output: "aaaccc"
+// Explanation: Both 'c' and 'a' appear three times, so both "cccaaa" and "aaaccc" are valid answers.
+// Note that "cacaca" is incorrect, as the same characters must be together.
+// Example 3:
+
+// Input: s = "Aabb"
+// Output: "bbAa"
+// Explanation: "bbaA" is also a valid answer, but "Aabb" is incorrect.
+// Note that 'A' and 'a' are treated as two different characters.
+
+
+class Solution {
+    public String frequencySort(String s) {
+        HashMap<Character, Integer> map=new HashMap<>();
+        for(char c:s.toCharArray()){
+            map.put(c,map.getOrDefault(c,0)+1);
+        }
+        
+        PriorityQueue<Character> heap=new PriorityQueue<>((a, b) -> 
+            map.get(b) - map.get(a));
+        heap.addAll(map.keySet());
+        StringBuilder sb = new StringBuilder();
+        while (!heap.isEmpty()) {
+            char c = heap.poll();
+            int count = map.get(c);
+            // Append the character 'count' number of times
+            for (int i = 0; i < count; i++) {
+                sb.append(c);
+            }
+        }    
+        String str=sb.toString(); 
+        return str;
+    }
+}
